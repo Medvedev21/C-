ed721ef1da91da269726a8ba4921cfe054c874d9
Medvedev21 <119859032+Medvedev21@users.noreply.github.com>
2022-12-05 14:46:35 +0300

    Add files via upload

---
+++ Оператор цикла с предусловием.cpp
@@ -0,0 +1,16 @@
+﻿#pragma warning(disable : 4996) // это строка отключает ошибку scanf
+#include <iostream>
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    printf("N:");
+    scanf("%i", &n);
+    int k !=0;
+    while ((k < n - 1) & ((n % k) != 0)) {
+        ++k;
+    }
+    printf("%s\n", ((n % k) != 0) ? "True" : "False");
+}
+
+
