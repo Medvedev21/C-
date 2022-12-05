c4cd8d0f01afcb4e2f681dc2e04feb6dda9af7f8
Medvedev21 <119859032+Medvedev21@users.noreply.github.com>
2022-12-05 14:48:47 +0300

    Add files via upload

---
+++ Одномерный массив.cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int a[10];
+    int n;
+
+    printf("N: ");
+    scanf("%i", &n);
+
+    int i;
+    for (i = 0; i < n; ++i) {
+        printf("a[%i] : ", i + 1);
+        scanf("%i", &a[i]);
+    }
+
+    int i2, k = 0, k2 = 1;
+    for (i = 0; i < n - 1; ++i) {
+        for (i2 = i + 1; i2 < n; ++i2) {
+            if (abs(a[i] - a[i2]) < abs(a[k] - a[k2])) {
+                k = i;
+                k2 = i2;
+            }
+        }
+    }
+    printf("%i %i\n", k + 1, k2 + 1);
+}
