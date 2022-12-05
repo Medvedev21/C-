81e435bb9fd56d6ea43fb021a93edd72f0a96248
Medvedev21 <119859032+Medvedev21@users.noreply.github.com>
2022-12-05 14:53:06 +0300

    Add files via upload

---
+++ Ввод и вывод данных.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	double A, B, x;
+
+	cout << "Vvedite znacheniye peremennoy A: ";
+	cin >> A;
+	cout << "Vvedite znacheniye peremennoy B: ";
+	cin >> B;
+	x = A;
+	A = B;
+	B = x;
+	cout << "Perviy sposob: " << endl; 
+	cout << "A: " << A << endl;
+	cout << "B: " << B << endl;
+
+	cout << "Vvedite znacheniye peremennoy A: ";
+	cin >> A;
+	cout << "Vvedite znacheniye peremennoy B: ";
+	cin >> B;
+	A = A + B;
+	B = A - B;
+	A = A - B;
+	cout << "Vtoroy sposob: " << endl;
+	cout << "A: " << A << endl;
+	cout << "B: " << B << endl;
+	return 0;
+}
