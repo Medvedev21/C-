18f6f20914dee49cec80fb7712144eac5a673c4c
Medvedev21 <119859032+Medvedev21@users.noreply.github.com>
2022-12-05 14:47:44 +0300

    Add files via upload

---
+++ Оператор цикла с параметрами.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    float X = 2;
+    int N = 22;
+    int switcher = 1;
+
+    float solve = 0;
+
+
+    for (int counter = 1; counter <= N; counter++) {
+        solve += switcher * pow(X, counter) / counter;
+        switcher *= -1;
+    }
+
+    cout << solve << endl;
+}
